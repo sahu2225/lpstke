138476bbc2a63c9044679624d21354e778f9c99a
jobayer1007 <62760527+jobayer1007@users.noreply.github.com>
2022-07-24 07:46:43 +0600

    total fixed

--- src/component/farms/Farms.jsx
+++ src/component/farms/Farms.jsx
@@ -93,32 +93,28 @@ const columns = [
       </div>
     ),
   },
-  {
-    title: '',
-    dataIndex: 'totalStake',
-    key: 'totalStake',
-    render: (totalStake) => {
-      console.log(totalStake);
-      let total = Promise.resolve(totalStake);
-      console.log(total);
-      total.then((totalStake) => {
-        console.log('value: ' + totalStake);
-        return totalStake;
-      });
-      return (
-        <div className='totalStaked'>
-          <p>Total Staked</p>
-          <span>
-            {/* $119,022,665 */}
-            {/* {totalStake} */}
-            <Tooltip title='Total value of the funds in this farm’s liquidity pool'>
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        </div>
-      );
-    },
-  },
+  // {
+  //   title: '',
+  //   dataIndex: 'totalStake',
+  //   key: 'totalStake',
+  //   render: (totalStake) => {
+  //     // totalStake.then((a) => {
+  //     return (
+  //       <div className='totalStaked'>
+  //         <p>Total Staked</p>
+  //         <span>
+  //           {/* $119,022,665 */}
+  //           {/* {totalStake} */}
+  //           <Tooltip title='Total value of the funds in this farm’s liquidity pool'>
+  //             <QuestionCircleOutlined />
+  //           </Tooltip>
+  //         </span>
+  //       </div>
+  //     );
+  //     // });
+  //     // return;
+  //   },
+  // },
   // {
   //   title: '',
   //   dataIndex: 'multiplier',
@@ -150,14 +146,6 @@ const data = [
     address: 'New York No. 1 Lake Park',
     description:
       'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-    totalStake: totalStakeCheck().then((res) => {
-      // setTotalStaked(res);
-      console.log(res);
-      // let totalStake = Promise.resolve(res);
-      // console.log(totalStake);
-
-      return res;
-    }),
   },
   // {
   //   key: 2,
@@ -192,11 +180,11 @@ const Farms = ({ conectmetaMask, accountDottedAddress }) => {
     });
   }, [hopeEarned]);
 
-  // useEffect(() => {
-  //   totalStakeCheck().then((res) => {
-  //     setTotalStaked(res);
-  //   });
-  // }, [totalStaked]);
+  useEffect(() => {
+    totalStakeCheck().then((res) => {
+      setTotalStaked(res);
+    });
+  }, [totalStaked]);
 
   const harvest = async () => {
     console.log('clicked harvest');
@@ -283,47 +271,63 @@ const Farms = ({ conectmetaMask, accountDottedAddress }) => {
                       <span className='d-none'>{record.description}</span>
                       <div className='row'>
                         <div className='col-md-4'>
-                          <div className='links'>
-                            <a
-                              href='http://www.google.com'
-                              target='_blank'
-                              rel='noreferrer'
-                            >
-                              Get Hope-BNB LP
-                              <svg
-                                viewBox='0 0 24 24'
-                                color='primary'
-                                width='20px'
-                                xmlns='http://www.w3.org/2000/svg'
-                                className='sc-eaf7e66-0 fJSxMQ'
-                              >
-                                <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
-                              </svg>
-                            </a>
-                            <a href='' target='_blank'>
-                              View Contract
-                              <svg
-                                viewBox='0 0 24 24'
-                                color='primary'
-                                width='20px'
-                                xmlns='http://www.w3.org/2000/svg'
-                                className='sc-eaf7e66-0 fJSxMQ'
-                              >
-                                <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
-                              </svg>
-                            </a>
-                            <a href='' target='_blank'>
-                              See Pair Info
-                              <svg
-                                viewBox='0 0 24 24'
-                                color='primary'
-                                width='20px'
-                                xmlns='http://www.w3.org/2000/svg'
-                                className='sc-eaf7e66-0 fJSxMQ'
-                              >
-                                <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
-                              </svg>
-                            </a>
+                          <div className='row'>
+                            <div className='col-md-6'>
+                              <div className='links'>
+                                <a
+                                  href='http://www.google.com'
+                                  target='_blank'
+                                  rel='noreferrer'
+                                >
+                                  Get Hope-BNB LP
+                                  <svg
+                                    viewBox='0 0 24 24'
+                                    color='primary'
+                                    width='20px'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='sc-eaf7e66-0 fJSxMQ'
+                                  >
+                                    <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
+                                  </svg>
+                                </a>
+                                <a href='' target='_blank'>
+                                  View Contract
+                                  <svg
+                                    viewBox='0 0 24 24'
+                                    color='primary'
+                                    width='20px'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='sc-eaf7e66-0 fJSxMQ'
+                                  >
+                                    <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
+                                  </svg>
+                                </a>
+                                <a href='' target='_blank'>
+                                  See Pair Info
+                                  <svg
+                                    viewBox='0 0 24 24'
+                                    color='primary'
+                                    width='20px'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='sc-eaf7e66-0 fJSxMQ'
+                                  >
+                                    <path d='M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z'></path>
+                                  </svg>
+                                </a>
+                              </div>
+                            </div>
+                            <div className='col-md-6'>
+                              <div className='totalStaked'>
+                                <p>Total Staked</p>
+                                <span>
+                                  {/* $119,022,665 */}
+                                  {totalStaked}{' '}
+                                  <Tooltip title='Total value of the funds in this farm’s liquidity pool'>
+                                    <QuestionCircleOutlined />
+                                  </Tooltip>
+                                </span>
+                              </div>
+                            </div>
                           </div>
                         </div>
                         <div className='col-md-4'>
