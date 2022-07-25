c81ae571aa1358f2ccf436fd1ccfc7a5e3ad2e30
sahu2225 <65921078+sahu2225@users.noreply.github.com>
2022-07-25 15:19:46 +0530

    Update Farms.jsx

--- src/component/farms/Farms.jsx
+++ src/component/farms/Farms.jsx
@@ -279,7 +279,7 @@ const Farms = ({ conectmetaMask, accountDottedAddress }) => {
                                   target='_blank'
                                   rel='noreferrer'
                                 >
-                                  Get Hope-BNB LP
+                                  Get Lotte-GDCC LP
                                   <svg
                                     viewBox='0 0 24 24'
                                     color='primary'
